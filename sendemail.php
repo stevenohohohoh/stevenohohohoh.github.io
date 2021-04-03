e345f4f51cd85223a2ae9efce314df7db21ff260
Steven <stevenohohohoh@gmail.com>
2021-04-03 23:02:52 +0900

    fixed and added stuff

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to             = 'themehippo@gmail.com'; //put your email here
+    $to             = 'stevenohohohoh@gmail.com'; //put your email here
     $email_template = 'simple.html';  // will find it on email-templates/ directory
 
     $subject    = "SUBJECT";
